55d63c343a39d9c81ce9beb193624292552bc322
Ashutosh Sameer <sameerashutosh0201@gmail.com>
2023-09-04 00:02:45 -0400

    Thread Pool

--- CH03/03_03/begin/thread_pool.py
+++ CH03/03_03/begin/thread_pool.py
@@ -2,11 +2,15 @@
 """ Chopping vegetables with a ThreadPool """
 
 import threading
+from concurrent.futures import ThreadPoolExecutor
 
 def vegetable_chopper(vegetable_id):
     name = threading.current_thread().getName()
     print(name, 'chopped vegetable', vegetable_id)
 
 if __name__ == '__main__':
+    pool = ThreadPoolExecutor(max_workers=5)
     for vegetable in range(100):
-        threading.Thread(target=vegetable_chopper, vegetable).start()
+        # threading.Thread(target=vegetable_chopper, args=(vegetable, )).start()
+        pool.submit(vegetable_chopper, vegetable)
+    pool.shutdown()

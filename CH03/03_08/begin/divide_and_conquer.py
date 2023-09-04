0f7a0a533afcd1fc3738f6af118f2e3a38b5989b
Ashutosh Sameer <sameerashutosh0201@gmail.com>
2023-09-04 00:35:30 -0400

    D & C

--- CH03/03_08/begin/divide_and_conquer.py
+++ CH03/03_08/begin/divide_and_conquer.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 """ Recursively sum a range of numbers """
 
-def recursive_sum(lo, hi):
-    if hi - lo <= 100_000: # base case threshold
-        return sum(range(lo, hi))
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+def recursive_sum(lo, hi, pool = None):
+    if not pool:
+        with ProcessPoolExecutor() as executor:
+            futures = recursive_sum(lo, hi, pool=executor)
+            return sum(f.result() for f in as_completed(futures))
     else:
-        mid = (hi + lo) // 2 # middle index for splitting
-        left = recursive_sum(lo, mid)
-        right = recursive_sum(mid, hi)
-        return left + right
+        if hi - lo <= 100_000: # base case threshold
+            return [pool.submit(sum, range(lo, hi))]
+        else:
+            mid = (hi + lo) // 2 # middle index for splitting
+            left = recursive_sum(lo, mid, pool=pool)
+            right = recursive_sum(mid, hi, pool=pool)
+            return left + right
 
 if __name__ == '__main__':
     total = recursive_sum(1, 1_000_000)
